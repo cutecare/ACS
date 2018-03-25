43eccf580734ae9522890dbcfaef3579f764938d
CuteCare <33788776+cutecare@users.noreply.github.com>
2018-03-25 08:39:19 +0300

    Create ACSensor.h

---
+++ ACSensor.h
@@ -0,0 +1,31 @@
+/*
+Part of http://cutecare.ru project
+Author: evgeny.savitsky@gmail.com
+*/
+
+#ifndef ACSensor_h
+#define ACSensor_h
+
+#if (ARDUINO >= 100)
+  #include <Arduino.h>
+#else
+  #include <WProgram.h>
+#endif
+
+// Coefficient to convert measured sinusoidal AC into effective AC
+#define AC_RMS_COEFF 0.707 
+
+class ACSensor {
+  public:
+	  ACSensor(float sensitivity = 40.0, float noise = 10.0);
+	  float getEffectiveVoltage();
+    float getEffectiveAmperes();
+    int getEffectiveWatt();
+
+  private:
+	  float _sensitivity = 0;
+    float _noise = 0;
+    float _zero = 0;
+};
+
+#endif
