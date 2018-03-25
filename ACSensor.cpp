5678d8f8040e2bbedf3edd5739299ca332dce625
CuteCare <33788776+cutecare@users.noreply.github.com>
2018-03-25 10:10:17 +0300

    Update ACSensor.cpp

--- ACSensor.cpp
+++ ACSensor.cpp
@@ -4,8 +4,9 @@ Author: evgeny.savitsky@gmail.com
 */
 #include "ACSensor.h";
 
-ACSensor::ACSensor(float sensitivity, float noise) 
+ACSensor::ACSensor(int pin, float sensitivity, float noise) 
 {
+  _pin = pin;
   _sensitivity = sensitivity;
   _noise = noise;
 }
@@ -17,32 +18,34 @@ void ACSensor::calibrate()
 
 float ACSensor::getVoltage()
 {
-  unsigned int maxValue = 0;
-  unsigned long averageValue = 0;
+  float averageValue = 0.0;
   int samples = 5;
   
+  pinMode(_pin, INPUT);
+  
   for( int j = 0; j < samples; j++ ) {
-    maxValue = 0;
+	unsigned int maxValue = 0;
     for( int i = 0; i < 20; i++ ) {
-      unsigned int readValue = abs(analogRead(SENSOR_PIN) - 512.0);
+      unsigned int readValue = analogRead(_pin);
+      readValue = abs(readValue - 512); // bi-directional ACS
       if ( readValue > maxValue ) {
         maxValue = readValue;
       }
       delay(2);
     }
-    averageValue += maxValue;
+    averageValue += (float) maxValue;
   }
-
-  return (float) (averageValue / (samples * 1024.0)) * 5000.0;
+  return 5000.0 * averageValue / ((float) samples * 1024.0);
 }
 
 float ACSensor::getAmperes()
 {
   float voltage = getVoltage();
+  voltage = abs(voltage - _zero);
   return (voltage <= _noise) ? 0.0 : (voltage * AC_RMS_COEFF / _sensitivity);
 }
 
-int ACSensor::getWatt(int voltage)
+int ACSensor::getWatt(float voltage)
 {
   return floor(getAmperes() * voltage);
 }
